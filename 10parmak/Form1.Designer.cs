19a1840dddf3b7cc1003e1f3c5edd3a62251949f
emre erol <emre_erol17@hotmail.com>
2024-06-12 20:39:30 +0300

    Add project files.

---
+++ 10parmak/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace _10parmak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(447, 310);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 40);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(447, 26);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(317, 244);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(796, 26);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(165, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(794, 74);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(190, 24);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "1.Seviyeden Başlasın";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(794, 118);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(142, 24);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Hata Sayacı  =";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(933, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "000";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 43);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Ayrıntılı Hata Sayısı";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(347, 246);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ 10parmak/Form1.cs
@@ -0,0 +1,239 @@
+﻿using _10parmak.Properties;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10parmak
+{
+    public partial class Form1 : Form
+    {
+
+        List<Button> b = new List<Button>();
+        
+        int j = 0;
+        int counter = 0;
+        Dictionary<char,int> istatistik= new Dictionary<char,int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCheckBoxValue();
+            comboBox1.Items.Add("1.seviye");
+            comboBox1.Items.Add("2.seviye");
+            comboBox1.Items.Add("3.seviye");
+            comboBox1.Items.Add("4.seviye");
+            comboBox1.Items.Add("5.seviye");
+
+            if (!checkBox2.Checked) label1.Visible = false;
+            label1.Font = new Font(label1.Font.FontFamily, 9);
+
+            // Combo Box
+            listBox1.Font = new Font(listBox1.Font.FontFamily, 14);
+            
+        }
+
+        public void Shuffle(char[] chars, int start, int end)
+        {
+            if (listBox1.Items.Count > 1) listBox1.Items.Clear();
+            Random r = new Random();
+            StringBuilder sb = new StringBuilder();
+            int counter = 0;
+
+
+
+            for (int i = 0; i < 10; i++) // adet
+            {
+                for (int j = 0; counter < r.Next(start, end); j++) // rastgele oluşturulsun
+                {
+                    sb.Append(chars[r.Next(0, chars.Length)]);
+                    counter++;
+                }
+                // 0 100 0-70 
+                listBox1.Items.Add(sb.ToString());
+                counter = 0;
+                sb.Clear();
+            }
+            listBox1.SelectedIndex = 0;
+            textBox1.Clear();
+        }
+
+        static void Info(char[] c,int start,int end)
+        {
+            string cToStr = string.Join(" ", c).ToUpper();
+            MessageBox.Show($"{cToStr} harflerinden oluşan ve {start} ile {end-1} uzunluğunda " +
+                $"rastgele kelimeler ile karşılacaksınız");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[] chars = { 'a', 's', 'd', 'f' };
+            int start = 2;
+                int end = 6;
+            Shuffle(chars, start, end);
+            textBox1.Focus();
+            Info(chars, start, end);
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[] chars = { 'j', 'k', 'l', 'ş' };
+            int start = 3;
+            int end = 7;
+            Info(chars, start, end);
+
+            Shuffle(chars, start, end);
+            textBox1.Focus();
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            char[] chars = { 'a', 's', 'd', 'f', 'j', 'k', 'l', 'ş' };
+            int start = 3;
+            int end = 7;
+            Info(chars, start, end);
+
+            Shuffle(chars, start, end);
+            textBox1.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            char[] chars = { 'a', 's', 'd', 'f', 'j', 'k', 'l', 'ş','g','h' };
+            int start = 3;
+            int end = 9;
+            Info(chars, start, end);
+
+            Shuffle(chars, start, end);
+            textBox1.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            char[] chars = { 'a', 's', 'd', 'f', 'j', 'k', 'l', 'ş', 'g', 'h','r','v','u','m' };
+            int start = 4;
+            int end = 12;
+            Info(chars, start, end);
+
+            Shuffle(chars, start, end);
+            textBox1.Focus();
+        }
+
+               
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = comboBox1.SelectedIndex;
+            lastV = i;
+            if (i == 0) button1_Click(sender,e);
+            else if (i==1) button2_Click(sender,e);
+            else if (i == 2) button3_Click(sender, e);
+            else if (i == 3) button4_Click(sender, e);
+            else if (i == 4) button5_Click(sender, e);
+        }
+
+        
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string s = listBox1.SelectedItem.ToString();
+            if (e.KeyChar != s[j])
+            {
+                if (checkBox2.Checked)
+                {
+                    if (istatistik.ContainsKey(s[j])) istatistik[s[j]]++;
+                    else istatistik.Add(s[j], 1);
+                    counter++; // hata sayacı
+                    label1_TextChanged(sender, e);
+                }
+                e.Handled = true;
+            }
+            else j++;
+            if (j == s.Length) // J sayacı kelimeye eşit mi?
+            {
+                if (listBox1.SelectedIndex + 1 >= listBox1.Items.Count)
+                {
+                    j = 0;
+                    e.Handled = true;
+                    textBox1.Clear();
+                    if (lastV + 1 < comboBox1.Items.Count)
+                    {
+                        comboBox1.SelectedIndex = lastV + 1;
+                        MessageBox.Show($"{lastV + 1}.seviye");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tebrikler..Daha fazla seviye yok.");
+                        listBox1.Items.Clear();
+                    }
+                }
+                else
+                {
+                    listBox1.SelectedIndex++;
+                    j = 0;
+                    e.Handled = true;
+                    textBox1.Clear();
+                }
+               
+            }
+        }
+        
+        private void LoadCheckBoxValue()
+        {
+            checkBox1.Checked = Properties.Settings.Default.CheckboxValue;
+            checkBox2.Checked = Properties.Settings.Default.CheckBoxSayac;
+
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.CheckboxValue = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) button1_Click(sender, e);
+            if (checkBox2.Checked) label1.Text = counter.ToString();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.CheckBoxSayac = checkBox2.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = counter.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DataTable ist = new DataTable();
+            ist.Columns.Add("Bu harfe basmak isterken");
+            ist.Columns.Add("yapılan yanlış tuş vuruşu");
+
+            foreach (KeyValuePair<char,int> item in istatistik)
+            {
+                ist.Rows.Add(item.Key, item.Value);
+            }
+           
+            dataGridView1.DataSource= ist;
+
+           
+            //MessageBox.Show(@"Tablo,aslında basmak istediğiniz tuşu yani bir sonraki harfe yanlış basıldığında kayıt yapar. " +
+                //"Örneğin \"F - 5\" yazıyorsa F'basmak isterken 5 kez yanlış tuşa bastığınızı gösterir. ");
+        }
+    }
+}
